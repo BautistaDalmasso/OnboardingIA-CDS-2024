bab9f45e80fce9c37f5ed3dd4edee0c2af3299d4
Bautista Dalmasso <j.bauti.d@gmail.com>
2024-03-27 12:59:16 -0300

    Fix import annoyances with `trainer.py`.

--- Backend/app/ai/trainer.py
+++ Backend/app/ai/trainer.py
@@ -1,30 +1,34 @@
 import numpy as np
 import tensorflow as tf
-import topics
 
-topics_ = topics.Topics()
-topics_.save_words()
+from app.ai.topics import Topics
+from app.file_paths import MODEL_PATH
 
-training_x, training_y = topics_.processed_training_data()
 
-model = tf.keras.models.Sequential()
-# Input layer.
-model.add(
-    tf.keras.layers.Dense(128, input_shape=(len(training_x[0]),), activation="relu")
-)
-# "Processing" layer.
-model.add(tf.keras.layers.Dropout(0.5))
-model.add(tf.keras.layers.Dense(64, activation="relu"))
-model.add(tf.keras.layers.Dropout(0.5))
-# Output layer.
-model.add(tf.keras.layers.Dense(len(training_y[0]), activation="softmax"))
+def train():
+    topics_ = Topics()
+    topics_.save_words()
 
-sgd = tf.keras.optimizers.SGD(
-    learning_rate=0.001, decay=1e-6, momentum=0.9, nesterov=True
-)
-model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
+    training_x, training_y = topics_.processed_training_data()
 
-train_process = model.fit(
-    np.array(training_x), np.array(training_y), epochs=100, batch_size=5, verbose=1
-)
-model.save("skynet.h5", train_process)
+    model = tf.keras.models.Sequential()
+    # Input layer.
+    model.add(
+        tf.keras.layers.Dense(128, input_shape=(len(training_x[0]),), activation="relu")
+    )
+    # "Processing" layer.
+    model.add(tf.keras.layers.Dropout(0.5))
+    model.add(tf.keras.layers.Dense(64, activation="relu"))
+    model.add(tf.keras.layers.Dropout(0.5))
+    # Output layer.
+    model.add(tf.keras.layers.Dense(len(training_y[0]), activation="softmax"))
+
+    sgd = tf.keras.optimizers.SGD(
+        learning_rate=0.001, decay=1e-6, momentum=0.9, nesterov=True
+    )
+    model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
+
+    train_process = model.fit(
+        np.array(training_x), np.array(training_y), epochs=100, batch_size=5, verbose=1
+    )
+    model.save(MODEL_PATH, train_process)
--- Backend/main.py
+++ Backend/main.py
@@ -1,13 +1,21 @@
 import uvicorn
 
+from app.ai.trainer import train
 from app.server_config import ServerConfig
 
 if __name__ == "__main__":
-    server_config = ServerConfig()
-
-    uvicorn.run(
-        "app.api:app",
-        host=server_config.get_server_ip(),
-        port=server_config.get_server_port(),
-        reload=True,
+    action = input(
+        "\n ------------------ \n0) Start server. \n1) Train model.\nSelect action: "
     )
+
+    if action == "1":
+        train()
+    else:
+        server_config = ServerConfig()
+
+        uvicorn.run(
+            "app.api:app",
+            host=server_config.get_server_ip(),
+            port=server_config.get_server_port(),
+            reload=True,
+        )
