94462d7f968fa2d7ed625e5126dcc1915b9606f1
paulinAPQ <alepaniaguapaulina@gmail.com>
2024-03-28 18:05:58 -0300

    se agrego drawer.Navigator en frontend

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,29 +1,46 @@
-import React from "react";
+import * as React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./src/components/Login";
 import Chat from "./src/components/Chat";
+import Form from "./src/components/Form";
 import { Routes } from "./src/common/enums/routes";
 import ChatHeader from "./src/components/ChatHeader";
 
+import {
+  createDrawerNavigator,
+ 
+} from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
+
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={Routes.Home}>
-        <Stack.Screen
-          name={Routes.Home}
-          component={Login}
-          options={{ header: () => null }}
-        />
-        <Stack.Screen
+    <>
+    <NavigationContainer >
+    <Drawer.Navigator  >
+    <Drawer.Screen name={Routes.Home} component={Login}  />
+  
+       <Drawer.Screen name="Formulario" component={Form} />
+       <Stack.Screen 
           name={Routes.Chat}
           component={Chat}
           options={{ header: (props) => <ChatHeader {...props} /> }}
         />
-      </Stack.Navigator>
+       </Drawer.Navigator>
     </NavigationContainer>
+    
+    </>
+  );
+};
+ 
+const FormNavigator = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Formulario" component={Form} />
+    </Drawer.Navigator>
   );
 };
 
