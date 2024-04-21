c8d809ab2464e47b1e7c0fb8548f6609807ac445
paulinAPQ <alepaniaguapaulina@gmail.com>
2024-04-21 13:08:56 -0300

    update home

--- Frontend/src/components/Home.tsx
+++ Frontend/src/components/Home.tsx
@@ -1,5 +1,5 @@
 import { NavigationProp } from "@react-navigation/native";
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { Routes } from "../common/enums/routes";
 import { useContextState } from "../ContexState";
@@ -10,9 +10,11 @@ interface Props {
 
 const Home = ({ navigation }: Props) => {
   const { contextState } = useContextState();
+  const [showSignup, setShowSignup] = useState(true);
+  const [showUnlock, setShowUnlock] = useState(false);
 
   const handleContinue = () => {
-    navigation.navigate(Routes.Chat);
+    navigation.navigate(Routes.Login);
   };
 
   const handleLogin = () => {
@@ -23,30 +25,66 @@ const Home = ({ navigation }: Props) => {
     navigation.navigate(Routes.Signup);
   };
 
+  const handleShowButtons = async () => {
+    setShowUnlock(true)
+    setShowSignup(false)
+  }
+
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.title}>¡Bienvenido al Chatbot!</Text>
+        <Text style={styles.title}>¡Bienvenido!</Text>
+        <Image
+          source={require("../assets/logo-skynet.png")}
+          style={styles.image} resizeMode="contain" />
         <Text style={styles.subtitle}>
-          {contextState.user ? `Hola ${contextState.user.firstName}. ` : ""}
-          Pregúntale a Skynet cualquier duda sobre la biblioteca
+          La mejor app para gestionar los libros de tu biblioteca
         </Text>
       </View>
-      <Image
-        source={require("../assets/login-image.png")}
-        style={styles.image}
-        resizeMode="contain"
-      />
+
+
       <View style={styles.buttonsContainer}>
         {contextState.accessToken === null ? (
           <>
             <View style={styles.loginButtonsContainer}>
-              <TouchableOpacity
-                style={styles.loginButton}
-                onPress={handleLogin}
-              >
-                <Text style={styles.loginButtonText}>Iniciar Sesión</Text>
-              </TouchableOpacity>
+              {showSignup &&
+                <TouchableOpacity
+                  style={styles.loginButton}
+                  onPress={handleShowButtons}
+                >
+                  <Text style={styles.loginButtonText}>Iniciar Sesión</Text>
+                </TouchableOpacity>
+              }
+              {showUnlock
+                &&
+                <View style={styles.unlockButtonsContainer}>
+
+                  <TouchableOpacity style={styles.unlockButton}
+                    onPress={handleLogin}>
+                    <Image
+                      source={require("../assets/fingerprint.png")}
+                      style={styles.iconUnlock}
+                      resizeMode="contain"
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.unlockButton}
+                    onPress={handleLogin}>
+                    <Image
+                      source={require("../assets/face.png")}
+                      style={styles.iconUnlock}
+                      resizeMode="contain"
+                    /></TouchableOpacity>
+                  <TouchableOpacity style={styles.unlockButton}
+                    onPress={handleLogin}>
+                    <Image
+                      source={require("../assets/password.png")}
+                      style={styles.iconUnlock}
+                      resizeMode="contain"
+                    />
+                  </TouchableOpacity>
+                </View>
+              }
+
               <TouchableOpacity
                 style={styles.signupButton}
                 onPress={handleSignup}
@@ -54,9 +92,11 @@ const Home = ({ navigation }: Props) => {
                 <Text style={styles.signupButtonText}>Registrarse</Text>
               </TouchableOpacity>
             </View>
+
             <Text style={styles.continueLink} onPress={handleContinue}>
-              {"Continuar sin iniciar sesión >>"}
+              {"Ingresar sin conexión >>"}
             </Text>
+
           </>
         ) : (
           <TouchableOpacity
@@ -79,64 +119,81 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffffff",
   },
   title: {
-    fontSize: 24,
+    marginTop: 200,
+    fontSize: 40,
     fontWeight: "bold",
-    marginBottom: 10,
+    color: "#098fea",
+
     textAlign: "center",
-    color: "#3369FF",
+    bottom: 15,
   },
   subtitle: {
-    fontSize: 16,
+    fontSize: 10,
     textAlign: "center",
-    paddingHorizontal: 10,
+    paddingHorizontal: 60,
+    bottom: 40,
+    color: "#191A1A",
   },
   image: {
-    height: 280,
+    height: 150,
+    bottom: 10,
+    width: 250,
+    alignSelf: "center",
   },
   buttonsContainer: {
+    marginTop: 200,
     flexDirection: "column",
     alignItems: "center",
-    width: "100%",
+    width: "80%",
+    bottom: 100,
   },
   loginButtonsContainer: {
-    flexDirection: "row",
     justifyContent: "center",
     width: "85%",
   },
   loginButton: {
-    backgroundColor: "#3369FF",
+    backgroundColor: "#098fea",
     paddingVertical: 10,
     paddingHorizontal: 20,
-    marginRight: 10,
+    marginBottom: 40,
     borderRadius: 100,
     borderWidth: 1,
-    borderColor: "#3369FF",
+    borderColor: "#098fea",
   },
   signupButton: {
+    marginBottom: 80,
     backgroundColor: "#ffffff",
-    borderColor: "#3369FF",
+    borderColor: "#098fea",
     borderWidth: 1,
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 100,
+
   },
   continueLink: {
-    color: "#3369FF",
-    marginTop: 25,
+    color: "#098fea",
+    marginTop: 50,
     textAlign: "center",
   },
-  loginButtonText: {
+   loginButtonText: {
     color: "#ffffff",
     fontSize: 16,
     fontWeight: "bold",
     textAlign: "center",
   },
   signupButtonText: {
-    color: "#3369FF",
+    color: "#098fea",
     fontSize: 16,
     fontWeight: "bold",
     textAlign: "center",
   },
+  unlockButtonsContainer: {
+    marginBottom: 40,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignSelf: "center",
+    width: "85%",
+  }, 
   continueButton: {
     backgroundColor: "#3369FF",
     paddingVertical: 10,
@@ -147,6 +204,19 @@ const styles = StyleSheet.create({
     borderColor: "#3369FF",
     width: "85%",
   },
+  unlockButton: {
+    backgroundColor: "#098fea",
+    borderRadius: 100,
+    borderWidth: 10,
+    borderColor: "#098fea",
+    margin: 10,
+  },
+  iconUnlock: {
+    width: 30,
+    height: 30,
+    alignSelf: "center",
+  },
+
 });
 
 export default Home;
