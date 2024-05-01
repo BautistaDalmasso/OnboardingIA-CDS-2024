cbd05e9f8e428f800f89c04c878d02473babbbdb
paulinAPQ <163665714+paulinAPQ@users.noreply.github.com>
2024-05-01 19:44:23 -0300

    Update book_loans_router.py

--- Backend/app/loan_management/book_loans_router.py
+++ Backend/app/loan_management/book_loans_router.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, HTTPException
 from app.loan_management import book_loans_service
 
-from .book_loans_dtos import LoanConfirmedDTO, RequestedBookDTO
+from .book_loans_dtos import LoanDTO, RequestedBookDTO
 
 router = APIRouter(prefix="/book", tags=["Book"])
 
 
 @router.post("confirmed_loan")
-async def create_confirmed_loan(book: LoanConfirmedDTO):
+async def create_confirmed_loan(book: LoanDTO):
     result = book_loans_service.add_confirmed_loan(book)
     if "error" in result:
         raise HTTPException(status_code=400, detail=result["error"])
