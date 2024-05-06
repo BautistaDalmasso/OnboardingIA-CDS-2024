2dac75ec49e73a0b2e6ce90d44eba70130a60aa4
paulinAPQ <alepaniaguapaulina@gmail.com>
2024-05-05 21:48:12 -0300

    Record-of-book-loan

--- Backend/tests/test_loans_service.py
+++ Backend/tests/test_loans_service.py
@@ -36,33 +36,3 @@ def test_add_requested_book_failure(loan_service):
     book = RequestedBookDTO(isbn="1234567890", copy_id="1", user_email="user@example.com")
     with pytest.raises(sqlite3.IntegrityError):
         loan_service.add_requested_book(book)
-def test_add_confirmed_loan_success():
-    book = LoanDTO(isbn="1234567890", copy_id="1", expiration_date="2023-03-15", user_email="user@example.com")
-    result = loan_service.add_confirmed_loan(book)
-    assert result == {
-        "isbn": book.isbn,
-        "copy_id": book.copy_id,
-        "expiration_date": book.expiration_date,
-        "user_email": book.user_email,
-    }
-
-def test_add_confirmed_loan_failure(loan_service):
-    book = LoanDTO(isbn="1234567890", copy_id="1", expiration_date="2023-03-15", user_email="user@example.com")
-    with pytest.raises(sqlite3.IntegrityError):
-        loan_service.add_confirmed_loan(book)
-
-def test_add_requested_book_success(loan_service):
-    book = RequestedBookDTO(isbn="1234567890", copy_id="1", user_email="user@example.com")
-    result = loan_service.add_requested_book(book)
-    assert result == {
-        "isbn": book.isbn,
-        "copy_id": book.copy_id,
-        "user_email": book.user_email,
-    }
-
-def test_add_requested_book_failure(loan_service):
-    book = RequestedBookDTO(isbn="1234567890", copy_id="1", user_email="user@example.com")
-    with pytest.raises(sqlite3.IntegrityError):
-        loan_service.add_requested_book(book)
-
-
