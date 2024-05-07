5c370f0ad480b56eb0faf6ea7e4259cabde1aed4
paulinAPQ <163665714+paulinAPQ@users.noreply.github.com>
2024-05-07 12:45:32 -0300

    consult-books-loans-by-email (#56)

--- Backend/app/loan_management/book_loans_router.py
+++ Backend/app/loan_management/book_loans_router.py
@@ -1,19 +1,20 @@
 from fastapi import APIRouter, Depends, HTTPException
-from library.library_service import add_requested_book, add_confirmed_loan
-from library.library_models import LoanDTO, RequestedBookDTO
+from .book_loans_service import LoanService
+from .book_loans_dtos import RequestedBookDTO, LoanDTO
 from fastapi.security import HTTPBearer
 from ..middlewares import verify_token
 from ..user.user_dtos import (
     TokenDataDTO,
 )
 from licence_levels.licence_service import consult_book_data
+from app.file_paths import DATABASE_PATH
 
 router = APIRouter(prefix="/loans", tags=["Loan"])
-
+loan_service = LoanService(DATABASE_PATH)
 
 @router.post("loan")
 async def create_confirmed_loan(book: LoanDTO):
-    return add_confirmed_loan(book)
+    return loan_service.add_confirmed_loan(book)
 
 
 @router.post("requested_book")
@@ -21,7 +22,7 @@ async def create_requested_book(book: RequestedBookDTO, token=Depends(HTTPBearer
     user_data: TokenDataDTO = await verify_token(token.credentials)
     book_level: int = consult_book_data(book.isbn)
     if book_level == user_data.licenceLevel:
-        result = add_requested_book(book)
+        result = loan_service.add_requested_book(book)
         if "error" in result:
             raise HTTPException(
                 status_code=400, detail=result["error en solicitar libro"]
@@ -30,3 +31,10 @@ async def create_requested_book(book: RequestedBookDTO, token=Depends(HTTPBearer
         raise HTTPException(
             status_code=401, detail="No tienes permisos para solicitar este libro"
         )
+
+
+@router.get("/user_loans")
+async def book_loans_by_user_email(email: str):
+    result = loan_service.consult_book_loans_by_user_email(email)
+    return result
+
