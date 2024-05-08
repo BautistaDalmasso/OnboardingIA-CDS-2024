8da8b755a2ec42c669c905e80b594057f200b6cf
Bautista Dalmasso <j.bauti.d@gmail.com>
2024-05-08 12:32:27 -0300

    Remove hardcoded book list.

--- Frontend/src/components/MyLoans.tsx
+++ Frontend/src/components/MyLoans.tsx
@@ -22,53 +22,6 @@ interface Loan {
 }
 
 const image = require("../assets/background.png");
-const bookList = [
-  {
-    id: "1",
-    name: "Rayuela",
-    fecha_vencimiento: "23-05-23",
-  },
-  {
-    id: "2",
-    name: "Cien años de soledad",
-    fecha_vencimiento: "13-04-23",
-  },
-  {
-    id: "4",
-    name: "Sobre héroes y tumbas",
-    fecha_vencimiento: "25-05-23",
-  },
-  {
-    id: "10",
-    name: "El amor en tiempos de cólera",
-    fecha_vencimiento: "01-04-23",
-  },
-  {
-    id: "14",
-    name: "Crónica de una muerte anunciada",
-    fecha_vencimiento: "02-06-23",
-  },
-  {
-    id: "1112",
-    name: "Martín fierro",
-    fecha_vencimiento: "23-07-23",
-  },
-  {
-    id: "165",
-    name: "Los ojos del perro siberiano",
-    fecha_vencimiento: "23-04-23",
-  },
-  {
-    id: "156",
-    name: "Metamorfosis",
-    fecha_vencimiento: "13-04-23",
-  },
-  {
-    id: "155",
-    name: "El señor de los anillos",
-    fecha_vencimiento: "23-05-23",
-  },
-];
 
 const MyLoans = () => {
   const { contextState } = useContextState();
