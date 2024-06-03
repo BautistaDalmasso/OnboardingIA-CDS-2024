65960b173489c5046c178540f6f66b2ee55e5e27
paulinAPQ <alepaniaguapaulina@gmail.com>
2024-06-03 17:28:16 -0300

    fix LoanManagementService

--- Frontend/src/services/LoanManagementService.ts
+++ Frontend/src/services/LoanManagementService.ts
@@ -1,11 +1,12 @@
-import { ServerAddress } from "../common/consts/serverAddress";
 import { baseFetch } from "./fetch";
 import { ILoanValid, IReservationRequest } from "../common/interfaces/Book";
 import { ILoanInformation } from "../common/interfaces/LoanReqResponse";
 import { LoanStatusCode } from "../common/enums/loanStatus";
+import { ServerAddress } from "../common/consts/serverAddress";
 
 export class LoanService {
   private static baseRoute: string = `${ServerAddress}loans`;
+
   constructor() {}
 
   static async requestBookReservation(
@@ -19,12 +20,73 @@ export class LoanService {
         data: book,
         token,
       });
-
       return response;
     } catch (error) {
       console.error("Error al realizar la solicitud:", error);
       throw new Error("Error al realizar la solicitud");
     }
+  }
+
+  static async setLoanStatusReserved(
+    loan_id: number,
+    due_date: string,
+    token: string
+  ): Promise<ILoanInformation> {
+    return baseFetch<{ loan_id: number; due_date: string }, ILoanInformation>({
+      token,
+      url: `${this.baseRoute}/set_status_reserved?loan_id=${loan_id}&due_date=${due_date}`,
+      method: "PATCH",
+      data: { loan_id, due_date },
+    });
+  }
+
+  static async setLoanStatusLoaned(
+    loan_id: number,
+    due_date: string,
+    token: string
+  ): Promise<ILoanInformation> {
+    return baseFetch<{ loan_id: number; due_date: string }, ILoanInformation>({
+      token,
+      url: `${this.baseRoute}/set_status_loaned?loan_id=${loan_id}&due_date=${due_date}`,
+      method: "PATCH",
+      data: { loan_id, due_date },
+    });
+  }
+
+  static async setLoanStatusReturned(
+    loan_id: number,
+    token: string
+  ): Promise<ILoanInformation> {
+    return baseFetch<{ loan_id: number }, ILoanInformation>({
+      token,
+      url: `${this.baseRoute}/set_status_returned?loan_id=${loan_id}`,
+      method: "PATCH",
+      data: { loan_id },
+    });
+  }
+
+  static async setLoanStatusReturnOverdue(
+    loan_id: number,
+    token: string
+  ): Promise<ILoanInformation> {
+    return baseFetch<{ loan_id: number }, ILoanInformation>({
+      token,
+      url: `${this.baseRoute}/set_status_returned_overdue?loan_id=${loan_id}`,
+      method: "PATCH",
+      data: { loan_id },
+    });
+  }
+
+  static async setLoanStatusReservationCanceled(
+    loan_id: number,
+    token: string
+  ): Promise<ILoanInformation> {
+    return baseFetch<{ loan_id: number }, ILoanInformation>({
+      token,
+      url: `${this.baseRoute}/set_status_reservation_Canceled?loan_id=${loan_id}`,
+      method: "PATCH",
+      data: { loan_id },
+    });
   }
 
   static async assignLoan(bookLoan: ILoanValid, token: string) {
@@ -62,65 +124,5 @@ export class LoanService {
       token,
     });
     return value;
-
-  static async setLoanStatusReserved(
-    loan_id: number,
-    due_date: string,
-    token: string,
-  ): Promise<ILoanInformation> {
-    return baseFetch<{ loan_id: number ; due_date : string}, ILoanInformation>({
-      token,
-      url: `${this.baseRoute}/set_status_reserved?loan_id=${loan_id}&due_date=${due_date}`,
-      method: "PATCH",
-      data: { loan_id, due_date},
-    });
-}
-
-  static async setLoanStatusLoaned(
-    loan_id: number,
-    due_date: string,
-    token: string,
-  ): Promise<ILoanInformation> {
-    return baseFetch<{ loan_id: number ; due_date : string}, ILoanInformation>({
-      token,
-      url: `${this.baseRoute}/set_status_loaned?loan_id=${loan_id}&due_date=${due_date}`,
-      method: "PATCH",
-      data: { loan_id, due_date},
-    });
-  }
-
-  static async setLoanStatusReturned(
-    loan_id: number,
-    token: string,
-  ): Promise<ILoanInformation> {
-    return baseFetch<{ loan_id: number}, ILoanInformation>({
-      token,
-      url: `${this.baseRoute}/set_status_returned?loan_id=${loan_id}`,
-      method: "PATCH",
-      data: { loan_id},
-    });
-  }
-
-  static async setLoanStatusReturnOverdue(
-    loan_id: number,
-    token: string,
-  ): Promise<ILoanInformation> {
-    return baseFetch<{ loan_id: number}, ILoanInformation>({
-      token,
-      url:  `${this.baseRoute}/set_status_returned_overdue?loan_id=${loan_id}`,
-      method: "PATCH",
-      data: { loan_id},
-    });
-  }
-  static async setLoanStatusReservationCanceled(
-    loan_id: number,
-    token: string,
-  ): Promise<ILoanInformation> {
-    return baseFetch<{ loan_id: number}, ILoanInformation>({
-      token,
-      url: `${this.baseRoute}/set_status_reservation_Canceled?loan_id=${loan_id}`,
-      method: "PATCH",
-      data: { loan_id},
-    });
   }
 }
