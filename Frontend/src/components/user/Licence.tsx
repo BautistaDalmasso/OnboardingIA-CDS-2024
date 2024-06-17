621589d68018b9b58240ba75deb98c62f3ee9203
noeliadiaz1 <nony_yoxxi@hotmail.com>
2024-06-17 17:19:30 -0300

    desing_Licence

--- Frontend/src/components/user/Licence.tsx
+++ Frontend/src/components/user/Licence.tsx
@@ -10,6 +10,8 @@ import {
 import { useContextState } from "../../ContexState";
 import { NavigationProp } from "@react-navigation/native";
 import { Routes } from "../../common/enums/routes";
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { AntDesign } from '@expo/vector-icons';
 
 interface Props {
   navigation: NavigationProp<any, any>;
@@ -17,183 +19,147 @@ interface Props {
 
 const Licence = ({ navigation }: Props) => {
   const { contextState } = useContextState();
-  const image = require("../../assets/LicenceBack.png");
+  const image = require("../../assets/headerLicence.png");
 
   function handleNavigateQR(): void {
     navigation.navigate(Routes.ViewQr);
   }
 
   return (
-    <View style={styles.containerBack}>
-      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-        <View style={styles.container}>
-          <View style={styles.header}>
-            <Text style={styles.title}>CARNET VIRTUAL</Text>
-          </View>
-
-          <View style={styles.containerLicence}>
-            <View style={styles.containerProfile}>
-              <View style={styles.containerData}>
-                <View style={styles.cardData}>
-                  <View style={styles.cardName}>
-                    {contextState.user && (
-                      <Text style={styles.name}>
-                        {contextState.user.firstName}{" "}
-                        {contextState.user.lastName}
-                      </Text>
-                    )}
-                  </View>
-                  <View style={styles.cardDni}>
-                    {contextState.user && (
-                      <Text style={styles.name}>
-                        DNI: {contextState.user.dni}
-                      </Text>
-                    )}
-                  </View>
-                  <View style={styles.cardMail}>
-                    {contextState.user && (
-                      <Text style={styles.name}>{contextState.user.email}</Text>
-                    )}
-                  </View>
-                </View>
-              </View>
-              <Image
-                source={require("../../assets/reader.png")}
-                style={styles.imageProfile}
-                resizeMode="contain"
-              />
-              <View style={styles.imageLogo}>
-                <Image
-                  source={require("../../assets/logo.png")}
-                  style={styles.imageLogo}
+    <View style={styles.screenContainer}>
+      <View style={styles.headerContainer}>
+        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <Text style={styles.titleHeader}>CARNET VIRTUAL</Text>
+        </ImageBackground>
+        <Image
+                  source={require("../../assets/reader.png")}
+                  style={styles.imageProfile}
                   resizeMode="contain"
                 />
-              </View>
-            </View>
+      </View>
+      <View style={styles.containerLicence}>
+          <View style={styles.cardName}>
+                  {contextState.user && (
+                    <Text style={styles.name}>
+                            {contextState.user.firstName}{" "}
+                            {contextState.user.lastName}
+                    </Text>
+                  )}
           </View>
+          <View style={styles.cardDni}>
+                  {contextState.user && (
+                    <Text style={styles.name}>
+                          DNI: {contextState.user.dni}
+                    </Text>
+                  )}
+          </View>
+          <View style={styles.cardMail}>
+                {contextState.user && (
+                  <Text style={styles.name}>{contextState.user.email}</Text>
+                )}
+          </View>
+           {/* TODO: prettier button. */}
+           <TouchableOpacity
+              onPress={handleNavigateQR}
+              style={styles.cardLink}
+            >
+               <AntDesign name="qrcode" size={wp('9%')} color="#056D8D" />
+              <Text style={styles.linkText}>Ver QR</Text>
+            </TouchableOpacity>
 
-          {/* TODO: prettier button. */}
-          <TouchableOpacity
-            onPress={handleNavigateQR}
-            style={styles.linkContainer}
-          >
-            <Text style={styles.linkText}>Ver QR</Text>
-          </TouchableOpacity>
-        </View>
-      </ImageBackground>
+          <View style={styles.imageLogo}>
+                  <Image
+                    source={require("../../assets/logo.png")}
+                    style={styles.imageLogo}
+                    resizeMode="contain"
+                  />
+
+      </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  containerBack: {
-    flex: 1,
-    width: "100%",
-    justifyContent: "center",
+  screenContainer: {
     alignItems: "center",
+    height: hp('100%'),
+    width: wp('100%'),
+    flex: 1,
+  },
+  headerContainer:{
+      height: hp('25%'),
+      width: wp('100%'),
+  },
+  titleHeader: {
+    fontSize:hp('3.5%'),
+    fontStyle: "italic",
+    fontWeight: "bold",
+    color: "#056D8D",
+    height: hp('15%'),
+    width: wp('100%'),
+    textAlign: "center",
+    textShadowRadius: 30,
+    textShadowColor: "#42FFD3",
+    textAlignVertical: "top",
   },
   image: {
     flex: 1,
     justifyContent: "center",
   },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  header: {
-    flex: 1,
-    width: "100%",
-    flexDirection: "row",
-    alignContent: "center",
-    alignItems: "center",
-    marginLeft: "30%",
-    justifyContent: "flex-start",
-  },
-  title: {
-    fontSize: 33,
-    marginRight: 90,
-    fontStyle: "italic",
-    fontWeight: "bold",
-    color: "#056D8D",
-  },
-  containerLicence: {
-    flex: 3,
-    alignItems: "center",
-  },
-  containerProfile: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: 350,
-    height: 480,
-  },
   imageProfile: {
     position: "absolute",
-    top: 0,
+    marginVertical: hp('18%'),
+    marginHorizontal: wp('38%'),
   },
-  containerData: {
+  containerLicence: {
     alignItems: "center",
-    top: 145,
-    justifyContent: "space-evenly",
-    width: 350,
-    height: 580,
+    height: hp('75%'),
+    width: wp('95%'),
     borderRadius: 15,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
+      height: 0.5,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 0.5,
+    elevation: 7,
+  },
+  imageLogo: {
+    width: wp('90%'),
+    flex: 1,
+    alignContent: "flex-end",
+    marginTop:hp('1%'),
+  },
+  cardName: {
+    flex: 0.7,
+    width: wp('80%'),
+    marginTop: hp('9%'),
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#51D7FF",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
       height: 2,
     },
     shadowOpacity: 0.5,
     shadowRadius: 3.84,
     elevation: 10,
-    padding: 10,
   },
-  imageLogo: {
-    top: 15,
-    width: 340,
-  },
-  cardData: {
-    alignItems: "center",
-    width: 350,
-    height: 320,
-  },
-
   name: {
-    fontSize: 22,
+    fontSize:wp('6%'),
     fontWeight: "bold",
     color: "#056D8D",
   },
-
-  cardName: {
-    flex: 1,
-    width: 280,
-    alignItems: "center",
-    justifyContent: "space-evenly",
-    backgroundColor: "#51D7FF",
-    borderRadius: 0,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 3.84,
-    elevation: 10,
-    padding: 5,
-  },
-
   cardDni: {
-    width: 280,
-    fontSize: 22,
-    color: "white",
-    marginTop: 50,
-    flex: 1,
-    top: 0,
+    width: wp('80%'),
+    marginTop: hp('3%'),
+    flex: 0.7,
     alignItems: "center",
-    justifyContent: "space-evenly",
-
+    justifyContent: "center",
     backgroundColor: "#51D7FF",
-    borderRadius: 0,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -202,20 +168,32 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.5,
     shadowRadius: 3.84,
     elevation: 10,
-    padding: 10,
   },
   cardMail: {
-    width: 280,
-    fontSize: 22,
-    color: "#666",
-    marginTop: 50,
-    flex: 1,
-    top: 0,
+    width: wp('80%'),
+    marginTop: hp('3%'),
+    flex: 0.7,
     alignItems: "center",
-    justifyContent: "space-evenly",
-
+    justifyContent: "center",
     backgroundColor: "#51D7FF",
-    borderRadius: 0,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
+    elevation: 10,
+  },
+  cardLink: {
+    flexDirection:"row",
+    width: wp('60%'),
+    color: "#666",
+    marginTop: wp('6%'),
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#51D7FF",
+    borderRadius: 10,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -232,9 +210,9 @@ const styles = StyleSheet.create({
     alignSelf: "center",
   },
   linkText: {
-    color: "blue",
-    textDecorationLine: "underline",
-    fontSize: 16,
+    fontSize: wp('5%'),
+    fontWeight: "bold",
+    color: "#056D8D",
   },
 });
 
